dabc5bbb4e34a38e286a546ed76e3e2a33cfb387
hemakas <hemaka.tropusi@gmail.com>
2022-03-28 09:33:34 +0530

    Delete modal error troubleshooting

--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -95,6 +95,29 @@
                             
                         </td>
                     </tr>
+
+                    {{-- delete confirmation modal --}}
+                    <div id="defaultModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center">
+                        <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
+                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                            <div class="p-6 space-y-6">
+                                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                Are you sure you want to delete this record?
+                                </p>
+                            </div>
+                            
+                            <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+                                <form action="{{ route('bookings.destroy', $booking) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button data-modal-toggle="defaultModal" type="sumbmit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Yes</button>
+                                </form>
+                                <button data-modal-toggle="defaultModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No</button>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+
                 @endforeach
             </tbody>
         </table>
@@ -104,30 +127,6 @@
 
     </div>
 
-    {{-- delete confirmation modal --}}
-    <div id="defaultModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center">
-        <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
-          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          
-          <div class="p-6 space-y-6">
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              Are you sure you want to delete this record?
-            </p>
-          </div>
-          
-          <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
-            <form action="{{ route('bookings.destroy', $booking) }}" method="post">
-                @csrf
-                @method('DELETE')
-                <button data-modal-toggle="defaultModal" type="sumbmit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Yes</button>
-            </form>
-            <button data-modal-toggle="defaultModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No</button>
-          </div>
-
-            
-                
-          </div>
-        </div>
-      </div>
+    
 
 </x-app-layout>
